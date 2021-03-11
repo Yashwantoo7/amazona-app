e5e2167b2375bed03beeb57c7639165bf4527125
unknown <yashwantcool007.com@gmail.com>
2021-03-11 11:26:23 +0530

    adding functionality to removeCart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import {useDispatch, useSelector} from 'react-redux';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import MessageBox from '../components/MessageBox';
 
 const CartScreen = (props) => {
@@ -18,6 +18,7 @@ const CartScreen = (props) => {
 
     const removeFromCartHandler=(id)=>{
         //delete action
+        dispatch(removeFromCart(id));
     }
     const checkoutHandler=()=>{
         //checkout
