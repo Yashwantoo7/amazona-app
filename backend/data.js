894109e82b059de37b1cf06144a78c01289ec8dd
unknown <yashwantcool007.com@gmail.com>
2021-03-11 14:28:30 +0530

    connected to mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users:[
+    {
+      name:'Yash',
+      email:'yash@gmail.com',
+      password:bcrypt.hashSync('1234',8),
+      isAdmin:true
+    },
+    {
+      name:'John',
+      email:'john@gmail.com',
+      password:bcrypt.hashSync('1234',8),
+      isAdmin:false
+    }
+  ],
     products: [
       {
         _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app=express();
+mongoose.connect(process.env.MONGODB_URL||'mongodb://localhost/amazona',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+});
 
 app.get('/',(req,res)=>{
     res.send("server is running");
@@ -19,6 +26,11 @@ app.get('/api/products/:id',(req,res)=>{
     }
 });
 
+app.use('/api/users',userRouter)
+
+app.use((err,req,res,next)=>{
+    res.status(500).send({message: err.message});
+})
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`server on PORT  ${PORT}`);
