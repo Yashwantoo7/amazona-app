fd1ae488ab45be266f3a6390d037ea35a8144e35
unknown <yashwantcool007.com@gmail.com>
2021-03-13 17:08:41 +0530

    added signin functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,16 +2,21 @@ import {createStore, compose, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userSigninReducer } from './reducers/userReducers';
 
 const initialState={
     cart:{
         cartItems:localStorage.getItem('cartItems')?JSON.parse(localStorage.getItem('cartItems')):[]
+    },
+    userSignin:{
+        userInfo:localStorage.getItem('userInfo')?JSON.parse(localStorage.getItem('userInfo')):null
     }
 };
 const reducer=combineReducers({
     productList:productListReducer,
     cart:cartReducer,
     productDetails:productDetailsReducer,
+    userSignin: userSigninReducer
 })
 
 const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
