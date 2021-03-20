b5c3fd168ec1499fae66036c312b77b0d15ad0d4
unknown <yashwantcool007.com@gmail.com>
2021-03-20 16:40:22 +0530

    aaded order api

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/users',userRouter);
 app.use('/api/products',productRouter);
+app.use('/api/orders',orderRouter);
 
 app.use((err,req,res,next)=>{
     res.status(500).send({message: err.message});
--- backend/utils.js
+++ backend/utils.js
@@ -6,3 +6,25 @@ export const generateToken = (user)=>{
         expiresIn: '30d',
     });
 }
+
+export const isAuth = (req,res,next)=>{
+    const authorization = req.headers.authorization;
+    if(authorization){
+        const token = authorization.slice(7, authorization.length); // Bearer xxxxxx
+        jwt.verify(
+            token,
+            porcess.env.JWT_SECRET || 'Somethingsecret',
+            (err,decode)=>{
+                if(err){
+                    res.status(401).send({message: 'Invalid Token'});
+                }else{
+                    req.user = decode;
+                    next();
+                }
+            }
+        )
+    }
+    else{
+        res.status(401).send({message: 'No Token'})
+    }
+}
