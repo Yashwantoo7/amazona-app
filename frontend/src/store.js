336c6584c1e4f6fab5ec4cde5d94fe730abfae0f
unknown <yashwantcool007.com@gmail.com>
2021-03-20 17:44:16 +0530

    added placeOrder functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
+import { orderCreateReducer } from './reducers/orderReducers';
 
 const initialState={
     cart:{
@@ -19,7 +20,8 @@ const reducer=combineReducers({
     cart:cartReducer,
     productDetails:productDetailsReducer,
     userSignin: userSigninReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    orderCreate: orderCreateReducer
 })
 
 const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
